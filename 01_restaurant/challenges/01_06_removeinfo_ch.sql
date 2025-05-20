01442727ac4f9e60a87ca30665a0556989a2ada4
AzaUchiha <166628634+AzaUchiha@users.noreply.github.com>
2025-05-20 13:29:35 +0000

    look up reservations

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,12 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+
+Select * from Reservations R
+join Customers C on R.CustomerID = C.CustomerID
+where FirstName like '%Norby'
+and Date > '2022-07-24'
+
+Delete from Reservations
+where ReservationID = 2000
