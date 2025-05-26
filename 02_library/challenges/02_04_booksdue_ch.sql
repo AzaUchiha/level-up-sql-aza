fcd0c35745bfecdad0012882b282659be49d698e
AzaUchiha <166628634+AzaUchiha@users.noreply.github.com>
2025-05-26 15:42:39 +0000

    latest

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,10 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+
+select p.FirstName, p.Email, b.Title, l.DueDate
+from Patrons p
+join Loans l on l.PatronID = p.PatronID
+join Books b on b.BookID = l.BookID
+where l.DueDate = '2022-07-13'
+and l.ReturnedDate is null
