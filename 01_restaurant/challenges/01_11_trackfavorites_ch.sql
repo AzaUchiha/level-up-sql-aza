df9019a00b476ea2337074798a7011f045dfe08c
AzaUchiha <166628634+AzaUchiha@users.noreply.github.com>
2025-05-26 10:23:22 +0000

    latest

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,17 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+
+--Get CustomerID (42)
+select * from Customers
+WHERE FirstName = 'Cleo'and LastName = 'Goldwater'
+
+
+UPDATE Customers
+SET FavoriteDish = (Select DishID from Dishes where Name = 'Quinoa Salmon Salad')
+WHERE CustomerID = 42
+
+select c.FirstName, c.LastName, c.FavoriteDish, d.Name  from Customers c
+join Dishes d on c.FavoriteDish = d.DishID
+where CustomerID =42
--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,22 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+with o as (
+select CustomerID, count(orderID) OrderCount
+from Orders
+group by CustomerID
+order by number_of_orders desc
+)
+
+select c.FirstName, c.LastName, c.Email, o.OrderCount
+join o on o.CustomerID = c.CustomerID
+;
+
+
+select c.FirstName, c.LastName, c.Email, count(orderID) OrderCount
+from Orders o
+join Customers c on o.CustomerID = c.CustomerID
+group by o.CustomerID
+order by OrderCount desc
+limit 15;
